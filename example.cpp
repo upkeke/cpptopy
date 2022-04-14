a6980d8530315961bfbf7d9c5fdab17230ab5d43
upkeke <storepy@qq.com>
2022-04-15 03:12:03 +0800

    1111

---
+++ example.cpp
@@ -0,0 +1,17 @@
+#include "example.h"
+
+void hello(string str)
+{
+	cout<<"hi  "<< str;
+}
+
+void introduce()
+{
+	string fr = "python";
+	hello(fr);
+	cout<<" "<<"³ôµÜµÜ"<<endl;
+}
+int add(int x, int y)
+{
+	return x+y;
+}
